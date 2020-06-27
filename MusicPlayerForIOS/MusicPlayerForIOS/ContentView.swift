963096210ae6748b8230098f8e97f6a8f2818a94
wuyunhui <wuyunhuiace@gmail.com>
2020-06-27 20:11:32 +0800

    ADD: ios project

---
+++ MusicPlayerForIOS/MusicPlayerForIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPlayerForIOS
+//
+//  Created by yunhui wu on 2020/6/27.
+//  Copyright © 2020 yunhui wu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
